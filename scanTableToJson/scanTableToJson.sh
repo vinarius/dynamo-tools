97d34d1e7ca68a8cd38b9207185746ca735876a6
Mark <vindevccm@gmail.com>
2020-11-17 11:38:28 -0500

    adjustments to gitignore

--- scanTableToJson/scanTableToJson.sh
+++
@@ -1,9 +0,0 @@
-AWS_PROFILE=personal \
-CLIENT=foo \
-PROJECT=bar \
-APPLICATION=baz \
-ENVIRONMENT= \
-SOURCE_TABLE_NAME=kraus \
-DESTINATION_TABLE_NAME=hhaex-dev-admin-table \
-REGION=us-east-1 \
-npx ts-node scanTableToJson.ts
